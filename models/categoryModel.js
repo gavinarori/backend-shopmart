0b64a15d493c5d377ac700f22f4725d217a94cdf
gavinarori <arorigavin@gmail.com>
2025-02-23 11:10:34 +0300

    updated: category contollers and models

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -1,37 +1,22 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require('mongoose')
 
 const categorySchema = new Schema({
     name: {
         type: String,
         required: true
     },
-    slug: {
-        type: String,
-        unique: true,
-        required: true
-    },
     image: {
         type: String,
         required: true
     },
-    bannerImage: {
-        type: String
-    },
-    parentCategory: {
-        type: Schema.Types.ObjectId,
-        ref: 'categories',
-        default: null
-    },
-    seo: {
-        title: { type: String },
-        description: { type: String }
-    },
-    featured: {
-        type: Boolean,
-        default: false
+    slug: {
+        type: String,
+        required: true
     }
-}, { timestamps: true });
+}, { timestamps: true })
 
-categorySchema.index({ name: 'text' });
+categorySchema.index({
+    name: 'text'
+})
 
-module.exports = model('categories', categorySchema);
+module.exports = model('categorys', categorySchema)
